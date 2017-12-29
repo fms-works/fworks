2058d6d0dc15469d33b682794dc5c58802d7d8d0
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-30 01:15:26 +0900

    create post work page

--- work/new.php
+++ work/new.php
@@ -14,49 +14,56 @@ if (empty($_SESSION['current_user_id'])) {
 ?>
 <?php include('../partial/top_layout.php'); ?>
 <div class="container">
-  <form method="post" action="post.php" enctype="multipart/form-data">
-    <div>
-      <label for="title">タイトル</label>
-      <input type="text" name="title" id="title">
-      <?php if($_SESSION['empty_title']) { ?>
-        <p>タイトルを入力してください</p>
-      <?php } ?>
+  <h1 class="py-3 my-4 page-title">作品を投稿する</h1>
+  <form method="post" action="post.php" enctype="multipart/form-data"></form>
+    <div class="form-group py-2">
+      <label for="title">タイトル <span class="note">*必須</span></label>
+      <input type="text" name="title" id="title" class="form-control" aria-describedby="titleHelp" placeholder="例) FMS Works">
+      <small id="titleHelp" class="form-text text-muted">作品の魅力が伝わるようなタイトルをつけましょう！</small>
     </div>
-    <div class="main_image">
-      <h2>メイン</h2>
+    <div class="form-group py-2 main_image">
+      <h3>メイン画像 <span class="note">*必須</span></h3>
       <input id="workImageInputMain" class="workImageInput" type="file" name="main_image" accept="image/jpg">
-      <label for="workImageInputMain" class="workImageOutput" aline="center"></label>
-      <?php if($_SESSION['empty_main_image']) { ?>
-        <p>メイン画像を登録してください</p>
-      <?php } ?>
+      <label for="workImageInputMain" class="workImageOutput" aline="center" aria-describedby="mainImageHelp"></label>
+      <small id="mainImageHelp" class="form-text text-muted">いい感じの写真をつけましょう！</small>
     </div>
-    <div>
-      <h2>サブ</h2>
-      <div class="sub_image">
+    <div class="form-group py-2 sub_images">
+      <h3>サブ画像</h3>
+      <div class="mr-2 sub_image">
         <input id="workImageInputSub1" class="workImageInput" type="file" name="sub_image1" accept="image/jpg">
         <label for="workImageInputSub1" class="workImageOutput" aline="center"></label>
       </div>
-      <div class="sub_image">
+      <div class="mr-2 sub_image">
         <input id="workImageInputSub2" class="workImageInput" type="file" name="sub_image2" accept="image/jpg">
         <label for="workImageInputSub2" class="workImageOutput" aline="center"></label>
       </div>
-      <div class="sub_image">
+      <div class="mr-2 sub_image">
         <input id="workImageInputSub3" class="workImageInput" type="file" name="sub_image3" accept="image/jpg">
         <label for="workImageInputSub3" class="workImageOutput" aline="center"></label>
       </div>
+      <small id="subImageHelp" class="form-text text-muted">写真が多いほうが魅力的です！</small>
     </div>
-    <div>
+    <div class="form-group py-2">
+      <label for="github-link">Githubリポジトリ</label>
+      <input type="text" name="github-link" id="link" class="form-control" aria-describedby="githubHelp" placeholder="例) https://github.com/user/repository">
+      <small id="githubHelp" class="form-text text-muted">自分の作品はどんどんGithubで全世界の人に公開しましょう！ → <a href="https://github.com" class="text-muted">https://github.com</a></small>
+    </div>
+    <div class="form-group py-2">
+      <label for="openprocessing-link">OpenProcessingのリンク</label>
+      <input type="text" name="openprocessing-link" id="link" class="form-control" placeholder="例) https://www.openprocessing.org/sketch/000000">
+      <small id="githubHelp" class="form-text text-muted">OpenProcessingを使うと、processingの作品をWeb上で動かして公開することができます！ → <a href="https://www.openprocessing.org/sketch/110105" class="text-muted">https://www.openprocessing.org/sketch/110105</a></small>
+    </div>
+    <div class="form-group py-2">
       <label for="link">リンク</label>
-      <input type="text" name="link" id="link">
+      <input type="text" name="link" id="link" class="form-control" placeholder="例) https://www.abcdef.com">
+      <small id="githubHelp" class="form-text text-muted">Webサービスとして公開してみましょう！</small>
     </div>
-    <div>
-      <label for="detail">詳細</label>
-      <textarea name="detail" id="detail" row="20" col="10"></textarea>
-      <?php if($_SESSION['empty_detail']) { ?>
-        <p>詳細を入力してください</p>
-      <?php } ?>
+    <div class="form-group py-2">
+      <label for="detail">詳細 <span class="note">*必須</span></label>
+      <textarea name="detail" id="detail" class="form-control" rows="5" placeholder="例) これはFMSの学生が自分の作品を自由に投稿してコメントし合えるSNSです。概要は..."></textarea>
+      <small id="detailbHelp" class="form-text text-muted">この作品がどんなものなのか、どういうところにこだわったのか説明しましょう！</small>
     </div>
-    <input type="submit" value="投稿する">
+    <button type="submit" class="btn btn-primary px-4 mb-5">投稿する</button>
   </form>
 </div>
 <?php include('../partial/bottom_layout.php'); ?>
