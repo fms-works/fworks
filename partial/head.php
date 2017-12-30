a7c831a38c174108f84aa60385703a5fb69db9c5
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-31 01:06:50 +0900

    file arrangement

--- partial/head.php
+++ partial/head.php
@@ -11,8 +11,6 @@ if (empty($title)) $title = 'タイトル';
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <!-- bootstrap -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-  <!-- tamplate -->
-  <!-- <link rel="stylesheet" href="<?php echo $path; ?>assets/css/template.min.css"> -->
   <link rel="stylesheet" href="<?php echo $path; ?>assets/css/style.css">
   <title><?php echo $title; ?></title>
 </head>
