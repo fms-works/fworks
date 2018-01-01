823dd68466d423f15584b9db89c7d22136cabec4
ebkn12 <ktennis.mqekr12@gmail.com>
2018-01-02 01:36:06 +0900

    modify user edit page

--- partial/footer.php
+++ partial/footer.php
@@ -1,3 +1,3 @@
 <footer class="navbar navbar-expand-lg navbar-light bg-white footer-border">
-  <p class="w-100 py-4 text-center text-secondary">&copy; 2018 Nakamura lab.</p>
+  <p class="w-100 py-4 text-center text-secondary">&copy; 2018 Nakamura lab. (B)</p>
 </footer>
--- user/edit.php
+++ user/edit.php
@@ -17,13 +17,26 @@ $user_data = get_user_data($pdo, $current_user_id);
 ?>
 <?php include('../partial/top_layout.php'); ?>
 <?php // ユーザー情報編集フォーム ?>
+<h2 class="py-4">ユーザー編集</h2>
 <form action="update.php" method="post">
-  <input type="text" name="name" value="<?php echo $user_data['name']; ?>">
-  <input type="text" name="github_account" value="<?php echo $user_data['github_account']; ?>">
-  <textarea name="profile" rows="" cols="">
-    <?php echo $user_data['profile']; ?>
-  </textarea>
-  <a href="show.php">キャンセル</a>
-  <input type="submit" value="変更する">
+  <div class="form-group py-2">
+    <label for="name">ユーザーネーム <span class="note">*必須</span></label>
+    <input type="text" id="name" name="name" class="form-control" aria-describedby="nameHelp" required value="<?php echo $user_data['name']; ?>" placeholder="例) 山田太郎">
+    <small id="nameHelp" class="form-text text-muted">20文字以内で登録してください</small>
+  </div>
+  <div class="form-group py-2">
+    <label for="github_account">Githubアカウント</label>
+    <input type="text" name="github_account" id="github_account" class="form-control" aria-describedby="githubAcountHelp" value="<?php echo $user_data['github_account']; ?>" placeholder="例) abc">
+    <small id="githubAcountHelp" class="form-text text-muted">Githunアカウントを登録しましょう！ → <a href="https://github.com" class="text-muted">https://github.com</a></small>
+  </div>
+  <div class="form-group py-2">
+    <label for="profile">プロフィール</label>
+    <textarea name="profile" id="profile" class="form-control" row="5" aria-describedby="profileHelp" placeholder="例) FMS B1の山田太郎です。3Dが好きです。よろしくお願いします！"><?php echo $user_data['profile']; ?></textarea>
+    <small id="profileHelp" class="form-text text-muted">簡単に自己紹介しましょう！</small>
+  </div>
+  <div class="d-flex justify-content-start mt-4 mb-5">
+    <a href="show.php" class="btn btn-warning text-dark mr-2">キャンセル</a>
+    <input type="submit" class="btn btn-info" value="変更する">
+  </div>
 </form>
 <?php include('../partial/bottom_layout.php'); ?>
