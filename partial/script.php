4a40da5928cedcefb90b8b48aa877a62c98fd4dd
ebkn12 <ktennis.mqekr12@gmail.com>
2018-01-01 22:01:11 +0900

    add lazy load script

--- partial/script.php
+++ partial/script.php
@@ -1,8 +1,10 @@
 <?php if (empty($path)) $path = ''; ?>
 <!-- jQuery -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!-- popper for bootstrap -->
+<!-- popper for Bootstrap -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" 。integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-<!-- bootstrap -->
+<!-- Bootstrap -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+<!-- Lazy Load -->
+<script src="https://cdn.jsdelivr.net/npm/lazyload@2.0.0-beta.2/lazyload.js"></script>
 <script src="<?php echo $path; ?>assets/js/application.js"></script>
